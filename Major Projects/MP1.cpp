d531ef37fbed47987761826fc1f3c1c9951931f4
Tw1ZZLER <ilekbrownies@gmail.com>
2023-09-25 00:23:25 -0400

    MP1 fix

--- Major Projects/MP1.cpp
+++ Major Projects/MP1.cpp
@@ -10,25 +10,30 @@
 
 using namespace std;
 
-int main() {
-    // Variable Declaration
-    int regularDonuts = 0;
-    int fancyDonuts = 0;
-    int appleFritters = 0;
-    double customerOwes;
-    double customerPays;
-    double changeDue;
-    int numberPennies = 0;
-    int numberNickels = 0;
-    int numberDimes = 0;
-    int numberQuarters = 0;
-    int numberDollars = 0;
-    const int DOLLAR_VALUE = 100;
-    const int QUARTER_VALUE = 25;
-    const int DIME_VALUE = 10;
-    const int NICKEL_VALUE = 5;
-    const int PENNY_VALUE = 1;
+int regularDonuts = 0;
+int fancyDonuts = 0;
+int appleFritters = 0;
+double customerOwes;
+double customerPays;
+double changeDue;
+int numberPennies = 0;
+int numberNickels = 0;
+int numberDimes = 0;
+int numberQuarters = 0;
+int numberDollars = 0;
+const double REGULAR_SINGLE = 0.75;
+const double REGULAR_DOZEN = 7.99;
+const double FANCY_SINGLE = 0.85;
+const double FANCY_DOZEN = 8.49;
+const double FRITTER_SINGLE = 1.50;
+const double SALES_TAX = 1.075;
+const int DOLLAR_VALUE = 100;
+const int QUARTER_VALUE = 25;
+const int DIME_VALUE = 10;
+const int NICKEL_VALUE = 5;
+const int PENNY_VALUE = 1;
 
+int main() {
     // Input
     cout << "Number of regular donuts ordered: ";
     cin >> regularDonuts;
@@ -38,17 +43,23 @@ int main() {
     cin >> appleFritters;
 
     // Calculate how much the customer owes
+    // Subtotal
+    customerOwes += ((regularDonuts / 12) * REGULAR_DOZEN) + ((regularDonuts % 12) * REGULAR_SINGLE);
+    customerOwes += ((fancyDonuts / 12) * FANCY_DOZEN) + ((fancyDonuts % 12) * FANCY_SINGLE);
+    customerOwes += appleFritters * FRITTER_SINGLE;
+    // Tax
+    customerOwes *= SALES_TAX;
 
-    cout << "Customer owes $";
-    cin >> customerOwes;
+    // Print owed and ask for payment
+    cout << "Customer owes $" << fixed << setprecision(2) << customerOwes << endl;
     cout << "Customer pays $";
     cin >> customerPays;
 
     // Change Due Calculation and Print
     changeDue = customerPays - customerOwes;
-    if (changeDue == 0) cout << "Exact payment recieved  - no change owed";
+    if (changeDue <= 0) cout << "Exact payment recieved  - no change owed";
     else cout << fixed << setprecision(2) << "Change due is $" << changeDue << " - ";
-    changeDue = round(changeDue * 100); // fixes double rounding bugs
+    changeDue = round(changeDue * 100); // converts cents to whole number, fixes a double bug
 
     // Calculate Numbers of Currencies
     // Dollars
@@ -64,6 +75,10 @@ int main() {
     }       
     // Quarters
     if (changeDue >= QUARTER_VALUE) {
+        // Add comma for list
+        if (numberDollars > 0) cout << ", ";
+
+        // Print number of quarters
         numberQuarters = changeDue / QUARTER_VALUE;
         cout << numberQuarters;
                
@@ -75,6 +90,10 @@ int main() {
     }
     // Dimes
     if (changeDue >= DIME_VALUE) {
+        // Add comma for list
+        if (numberQuarters || numberDollars > 0) cout << ", ";
+
+        // Print number of quarters
         numberDimes = changeDue / DIME_VALUE;
         cout << numberDimes;
                
@@ -86,6 +105,10 @@ int main() {
     }
     // Nickels
     if (changeDue >= NICKEL_VALUE) {
+        // Add comma for list
+        if (numberDimes || numberQuarters || numberDollars > 0) cout << ", ";
+
+        // Print number of quarters
         numberNickels = changeDue / NICKEL_VALUE;
         cout << numberNickels;
                
@@ -97,6 +120,10 @@ int main() {
     }
     // Pennies
     if (changeDue >= PENNY_VALUE) {
+        // Add comma for list
+        if (numberNickels || numberDimes || numberQuarters || numberDollars > 0) cout << ", ";
+
+        // Print number of quarters
         numberPennies = changeDue / PENNY_VALUE;
         cout << numberPennies;
                
@@ -105,4 +132,5 @@ int main() {
         else cout << " pennies"; // plural print
     }
     cout << ".";
+    return 0;
 }

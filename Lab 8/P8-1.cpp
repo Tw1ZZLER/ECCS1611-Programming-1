b241ca12c83ed8b081480f91291c941155f8cad8
Tw1ZZLER <ilekbrownies@gmail.com>
2023-10-19 12:47:22 -0400

    Completed countVowel function

--- Lab 8/P8-1.cpp
+++ Lab 8/P8-1.cpp
@@ -6,9 +6,21 @@ int countCharacter(string line);
 int countVowel(string line);
 
 int main() {
-  
-    }
+    string line = "";
+
 }
 int countCharacter(string line) {
     return line.length();
+}
+int countVowel(string line) {
+    int vowels = 0;
+    for(int i = 0; i < line.length(); ++i) {
+        if(line[i]=='a' || line[i]=='e' || line[i]=='i' ||
+           line[i]=='o' || line[i]=='u' || line[i]=='A' ||
+           line[i]=='E' || line[i]=='I' || line[i]=='O' ||
+           line[i]=='U') {
+            ++vowels;
+        }
+    }
+    return vowels;
 }

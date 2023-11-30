2ef161996d7b826d32316dba151a2ae268d19df4
Tw1ZZLER <ilekbrownies@gmail.com>
2023-11-30 13:09:14 -0500

    Completed P13B-1

--- Labs/Lab 13/P13B-1.cpp
+++ Labs/Lab 13/P13B-1.cpp
@@ -21,8 +21,10 @@ int main() {
         cout << endl;
     }
 }
+
 bool isLeapYear(int year) {
     if((year % 4 == 0) && !(year % 100 == 0)) return true;
     else if((year % 100 == 0) && (year % 400 == 0)) return true;
     else return false;
 }
+//done
